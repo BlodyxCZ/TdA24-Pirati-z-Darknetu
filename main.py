4192ff8e011e25edf0dee4badbe290c4bc635571
DavidCZ2051 <84991177+DavidCZ2051@users.noreply.github.com>
2023-12-15 01:19:45 +0100

    Reestablishes DB connection when closed

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import asyncio
 import sass
 import logging
 
+loop = asyncio.get_event_loop()
+app = Flask(__name__)
+
 # SCSS
 
 sass.compile(dirname=('static/scss', 'static/css'), output_style='compressed')
 
-loop = asyncio.get_event_loop()
-app = Flask(__name__)
-
-
 # CSS
+
 
 @app.route("/css/styles.css")
 def css():
@@ -21,9 +21,11 @@ def css():
 
 # Pages
 
+
 """ @app.route("/")
 def index():
     return render_template("index.html") """
+
 
 @app.route("/lecturer")
 def lecturer():
@@ -31,15 +33,18 @@ def lecturer():
 
 # API
 
+
 """ @app.route("/api")
 def api():
     # Portal reference
     return {"secret": "The cake is a lie"}, 200 """
 
+
 @app.route("/api/lecturers", methods=["GET"])
 def get_lecturers():
     lecturers = db.get_lecturers()
     return lecturers, 200
+
 
 @app.route("/api/lecturers/<uuid>", methods=["GET"])
 def get_lecturer(uuid):
@@ -48,10 +53,12 @@ def get_lecturer(uuid):
         return {"code": 404, "message": "User not found"}, 404
     return lecturer, 200
 
+
 @app.route("/api/lecturers", methods=["POST"])
 def post_lecturer():
     response = db.post_lecturer(request.get_json())
     return response, 201
+
 
 @app.route("/api/lecturers/<uuid>", methods=["PUT"])
 def put_lecturer(uuid):
@@ -59,6 +66,7 @@ def put_lecturer(uuid):
     if lecturer == None:
         return {"code": 404, "message": "User not found"}, 404
     return lecturer, 200
+
 
 @app.route("/api/lecturers/<uuid>", methods=["DELETE"])
 def delete_lecturer(uuid):
@@ -69,6 +77,18 @@ def delete_lecturer(uuid):
 
 # Server utilities
 
+
+@app.before_request
+def check_db_connection_before_request():
+    loop.run_until_complete(db.init())
+
+
+@app.route("/api/conn")
+def check_db_connection():
+    loop.run_until_complete(db.init())
+    return {"msg": "Tried to renew connection."}, 200
+
+
 @app.route("/api/log")
 def log():
     with open("logs.log", "r") as file:
@@ -78,6 +98,7 @@ def log():
 def exit_handler() -> None:
     print("Closing database connection...")
     loop.run_until_complete(db.close())
+
 
 def main() -> None:
     logging.basicConfig(filename="logs.log", filemode="w", format="%(name)s → %(levelname)s: %(message)s<br>")
@@ -91,5 +112,6 @@ def main() -> None:
     print("Starting webserver...")
     app.run(port=80, host="0.0.0.0")
 
+
 if __name__ == "__main__":
-   main()
+    main()
