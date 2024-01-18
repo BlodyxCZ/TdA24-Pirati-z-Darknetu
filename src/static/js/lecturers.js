37e2b07a3ebd45dd98c82d37a8c9e8dd6d35916d
ondradoksy <ondradoksy@ondradoksy.com>
2024-01-18 19:25:28 +0100

    Added slider

--- src/static/js/lecturers.js
+++ src/static/js/lecturers.js
@@ -1,5 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
     getLecturers();
+
+    let sliderOne = document.getElementById("price-min");
+    let sliderTwo = document.getElementById("price-max");
+    let minGap = 0;
+    let sliderTrack = document.querySelector(".slider-track");
+    let sliderMaxValue = document.getElementById("price-min").max;
+
+    function fillColor() {
+        percent1 = (sliderOne.value / sliderMaxValue) * 100;
+        percent2 = (sliderTwo.value / sliderMaxValue) * 100;
+        sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`;
+    }
+
+    sliderOne.addEventListener("input", (e) => {
+        if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+            sliderOne.value = parseInt(sliderTwo.value) - minGap;
+        }
+        //displayValOne.textContent = sliderOne.value;
+        fillColor();
+    });
+    sliderTwo.addEventListener("input", (e) => {
+        if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+            sliderTwo.value = parseInt(sliderOne.value) + minGap;
+        }
+        //displayValTwo.textContent = sliderTwo.value;
+        fillColor();
+    });
 });
 
 async function getLecturers() {
